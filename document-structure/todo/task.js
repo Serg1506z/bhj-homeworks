b49c753755677441bd0a25f1d2d15961fbbaa098
SergeyZ <81sergejjzabelkin@gmail.com>
2023-11-29 14:00:43 +0300

    Изменение структуры HTML-документа 2

---
+++ document-structure/todo/task.js
@@ -0,0 +1,24 @@
+let input = document.querySelector("#task__input");
+let button = document.querySelector("#tasks__add")
+let taskList = document.querySelector("#tasks__list");
+
+function addTask (e){
+    e.preventDefault();
+    let text = input.value;
+    input.value = "";
+
+    let element = document.createElement("div");
+    element.className = "task";
+    element.innerHTML = `<div class="task__title">${text}</div>
+    <a href="#" class="task__remove">&times;</a>`;
+
+    element.querySelector(".task__remove").addEventListener("click",(e) =>{
+        e.preventDefault();
+        taskList.removeChild(element);
+    })
+    taskList.append(element)
+}
+
+
+
+button.addEventListener("click",addTask);
