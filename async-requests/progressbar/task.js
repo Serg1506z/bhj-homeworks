8aa0cf11046022b382078ffe96b8529f5b1da939
SergeyZ <81sergejjzabelkin@gmail.com>
2023-12-11 17:02:43 +0300

    «Асинхронные запросы». Загрузка больших файлов. 3 исправление

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,14 +1,14 @@
 let form = document.querySelector("#form");
 let progress = document.querySelector("#progress");
-let file = document.querySelector("#file");
+let fileInput = document.querySelector("#file");
 
 form.addEventListener("submit",(e) => {
     e.preventDefault();
-    let f = file.files[0];
+    let file = fileInput.files[0];
     let xhr = new XMLHttpRequest();
     let formSent = new FormData();
-    if(file.files.length > 0){
-        formSent.append("file",f);
+    if(fileInput.files.length > 0){
+        formSent.append("file",file);
         xhr.upload.addEventListener("progress",(event) =>{
             let load = event.loaded/event.total;
             progress.value = load;
